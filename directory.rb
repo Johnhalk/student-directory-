cabe69851de8b727cd7464054a66e1140db49387
John Ashton <John_ashton@hotmail.com>
2016-12-11 12:16:47 +0000

    Turning our arrays into hashes

--- directory.rb
+++ directory.rb
@@ -3,27 +3,28 @@
 student_count = 11
 
 students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffery Baratheon",  # he WOULD be there.
- "Norman Bates"
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex DeLarge", cohort: :november},
+ {name: "The Wicked Witch of the West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddy Krueger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Joffery Baratheon", cohort: :november}, # he WOULD be there.
+ {name: "Norman Bates", cohort: :november}
 ]
-
+# replace the [] brackets with {} for hashes
 def print_header
   puts "The students of Villains Academy"
   puts " ---------------- "
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    #by turning our arrays into hashes we can create a clearer code for reading purposes, now it is clear that each variable is name and cohort.
   end
 end
 
