e7317a75b694eaea437a3d659a1e8d4533b91672
John Ashton <John_ashton@hotmail.com>
2016-12-11 12:29:13 +0000

    Adding user interactivity

--- directory.rb
+++ directory.rb
@@ -15,11 +15,30 @@ students = [
  {name: "Joffery Baratheon", cohort: :november}, # he WOULD be there.
  {name: "Norman Bates", cohort: :november}
 ]
-# replace the [] brackets with {} for hashes
+# replace the [] array brackets with {} for hashes
 def print_header
   puts "The students of Villains Academy"
   puts " ---------------- "
 end
+
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from user
+    name = gets.chomp
+  end
+  #return the artray of students
+  students
+  end
 
 def print(students)
   students.each do |student|
@@ -33,7 +52,7 @@ def print_footer(names)
 end
 
 # now we must call upon the methods
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
