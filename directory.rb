eb7c89efad4c8ea32c8dda64cdaa7696c827b228
John Ashton <John_ashton@hotmail.com>
2016-12-11 09:54:59 +0000

    Add a list of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#first we print the list of students
+
 puts "The student of Villains Academy"
 puts "-----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +13,12 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffery Baratheon"  # he WOULD be there.
 puts "Norman Bates"
+
+#finally we print the total number of students with the puts command
+print "Overall we have "
+
+#it's important that print() doesn't add new line characters
+
+print 11
+
+puts " great students!"
