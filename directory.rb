cd4782c4b9c778c19d91db820c92ed338c867dc1
John Ashton <John_ashton@hotmail.com>
2016-12-11 19:21:01 +0000

    ARGV added

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
    print_menu
-   process(gets.chomp)
+   process($stdin.gets.chomp)
  end
  end
 
@@ -11,7 +11,7 @@ def print_menu
   # 1. Print the menue and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to file tsudents.csv"
+  puts "3. Save the list to file students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit" #9 because we will add more items
 end
@@ -44,14 +44,14 @@ def input_students
   puts "To finish, just hit return twice"
 
   # get the first name
-  name = gets.chomp
+  name = $stdin.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = $stdin.gets.chomp
   end
 end
 
@@ -82,7 +82,7 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r") #open file, in read format only
   file.readlines.each do |line| #for each line in file do the following
     name, cohort = line.chomp.split(',')
@@ -91,5 +91,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #gets out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    Exit
+  end
+end
 
+try_load_students
 interactive_menu
