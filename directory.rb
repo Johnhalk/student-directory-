c9d4762d3fb071bfac055b62a5b6c38c5a338806
John Ashton <John_ashton@hotmail.com>
2016-12-11 13:55:50 +0000

    8.2 complete

--- directory.rb
+++ directory.rb
@@ -16,27 +16,37 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
+
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     #get another name from user
     name = gets.chomp
   end
-  #return the artray of students
+
+  #return the array of students
+
   students
   end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    #by turning our arrays into hashes we can create a clearer code for reading purposes, now it is clear that each variable is name and cohort.
+  def print(students)
+     puts "Please enter the first letter of the student's name:"
+     letter = gets.chomp.downcase
+     select_students = students.select { |student| student[:name][0] == letter }
+      select_students.each.with_index(1) do |student, i|
+        puts "#{i}: #{select_students[0]} (#{student[:cohort]} cohort)"
   end
 end
+
 
 def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 
+def short_names(students)
+  puts students[:name].length
+end
 # now we must call upon the methods
+
 students = input_students
 print_header
 print(students)
